9a4d1f5665ee15b51a46bcd35456dd682fb415cc
devfabien <ish001fabi@gmail.com>
2023-10-09 17:37:28 +0200

    Password Penalty removing the comments

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -1,16 +1,8 @@
-/**
- * Calculates the sum of penalty points for a given password.
- * Double characters like `aa` count as 1 penalty point, triples and more are 2 points.
- * It returns the sum of penalty points for a password or 0.
- * @param {string} password
- * @returns {number}
- */
-export default function penaltyPoints(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
+export default function penaltyPoints(password) {
   if (typeof password !== "string") password = String(password);
-
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+  const REG_EXP=/(.)\1+/g
+  if(password === 'null' || password.trim() === '') return 0;
+  const matches = password.match(REG_EXP);
+  if (!matches) return 0;
+  return matches.reduce((count, cur) => count + (cur.length > 2 ? 2 : 1), 0);
 }
