3571e536f6df4c4a861c1b825c398ed6101223ac
devfabien <ish001fabi@gmail.com>
2023-10-09 21:06:25 +0200

    Password penality improving the naming convention

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -1,8 +1,8 @@
 export default function penaltyPoints(password) {
   if (typeof password !== "string") password = String(password);
-  const REG_EXP=/(.)\1+/g
-  if(password === 'null') return 0; 
-  const matches = password.match(REG_EXP);
-  if (!matches) return 0;
-  return matches.reduce((count, cur) => count + (cur.length > 2 ? 2 : 1), 0);
+  const REG_EXP = /(.)\1+/g;
+  if (password === "null") return 0;
+  const MATHCES = password.match(REG_EXP);
+  if (!MATHCES) return 0;
+  return MATHCES.reduce((count, cur) => count + (cur.length > 2 ? 2 : 1), 0);
 }
