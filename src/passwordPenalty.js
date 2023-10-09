2871e568ef8694cfe851ccd1b2dda12c5a55a37a
devfabien <ish001fabi@gmail.com>
2023-10-09 17:41:08 +0200

    Password Penalty refactoring codes

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -1,7 +1,7 @@
 export default function penaltyPoints(password) {
   if (typeof password !== "string") password = String(password);
   const REG_EXP=/(.)\1+/g
-  if(password === 'null' || password.trim() === '') return 0;
+  if(password === 'null') return 0; 
   const matches = password.match(REG_EXP);
   if (!matches) return 0;
   return matches.reduce((count, cur) => count + (cur.length > 2 ? 2 : 1), 0);
