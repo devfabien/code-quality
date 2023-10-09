0bb988dbebd5c02c96e28e03e92c2c190efc18d8
devfabien <ish001fabi@gmail.com>
2023-10-09 16:52:51 +0200

    Improvement exercise2 removing the comments

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -1,10 +1,3 @@
-/**
- *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
- *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
- *  PLEASE DO NOT REMOVE `export default function`
- */
-
-// FUNCTION 2
 export default function filterFoods(data) {
   return data.filter((data) => data.type === "Food");
 }
