9dfa22de99211a79ed1d08cbffdc78f19aeea3a8
devfabien <ish001fabi@gmail.com>
2023-10-09 16:25:16 +0200

    Passwordvalidation checking if it is not among the forbidden password

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -9,7 +9,7 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 export default function isValidPassword(password = "") {
   if (typeof password !== "string") password = String(password);
  const REG_EXP=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{10}$/
- if(REG_EXP.test(password) && new Set([...password]).size>3) return true
+ if(REG_EXP.test(password) && new Set([...password]).size>3&& !forbiddenPasswords.includes(password)) return true
  return false
   // * * * YOUR CODE GOES IN HERE ... * * *
   /*
