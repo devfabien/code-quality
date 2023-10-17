845a282bae35e81962ebfc7090190044d56a3b72
devfabien <ish001fabi@gmail.com>
2023-10-17 10:53:47 +0200

    Password Validation removing nested functions

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -3,24 +3,33 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 export default function isValidPassword(password) {
   if (typeof password !== "string") password = String(password);
 
+  /* check if the password contains lowercase letters, uppercase letters, numbers 
+  and also the length of the passowrd is only 10 characters */
+  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{10}$/.test(password))
+    return false;
+
   if (new Set([...password]).size < 4) return false;
 
   if (forbiddenPasswords.includes(password)) return false;
 
-  /* check if the password contains lowercase letters, uppercase letters, numbers 
-  and also the length of the passowrd is 10 */
-  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{10}$/.test(password)) return false;
-  
-  //the following line returns numbers array from the password
-  const NUMBERS_ARRAY = password.match(/\d+/g);
-  const DIGIT_ARRAYS = NUMBERS_ARRAY.map((number) =>
-    number.toString().split("").map(Number)
-  );
-
-  const MERGED_ARRAY = [].concat(...DIGIT_ARRAYS);
-  return !MERGED_ARRAY.some(
-    (number, index) =>
-      number + 1 == MERGED_ARRAY[index + 1] ||
-      number - 1 == MERGED_ARRAY[index + 1]
-  );
+  const consecutiveNumbers = [
+    "012",
+    "123",
+    "234",
+    "345",
+    "456",
+    "567",
+    "678",
+    "789",
+    "987",
+    "876",
+    "765",
+    "654",
+    "543",
+    "432",
+    "321",
+    "210",
+  ].some((numbers) => password.includes(numbers));
+  if (consecutiveNumbers) return false;
+  return true;
 }
