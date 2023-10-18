c5c0390fb21b87a9b99e679168cf834cf9e80f57
devfabien <ish001fabi@gmail.com>
2023-10-18 11:56:39 +0200

    removing the typo error

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -2,10 +2,10 @@ export default function penaltyPoints(password) {
   if (password === null) return 0;
   if (typeof password !== "string") password = String(password);
   //regex to return array of duplicate values
-  const REG_EXP = /(.)\1+/g;
+  const regExp = /(.)\1+/g;
  
-  const MATHCES = password.match(REG_EXP);
-  if (!MATHCES) return 0;
+  const matches = password.match(regExp);
+  if (!matches) return 0;
   
-  return MATHCES.reduce((count, cur) => count + (cur.length > 2 ? 2 : 1), 0);
+  return matches.reduce((count, cur) => count + (cur.length > 2 ? 2 : 1), 0);
 }
